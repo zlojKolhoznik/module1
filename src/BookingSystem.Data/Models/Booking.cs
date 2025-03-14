880dee450abb42ce83d557b665c7603df1b0e87b
Roman Portianko <rportyanko10@gmail.com>
2025-03-14 16:56:17 +0200

    Added booking entity class

---
+++ src/BookingSystem.Data/Models/Booking.cs
@@ -0,0 +1,51 @@
+// <copyright file="Booking.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace BookingSystem.Data.Models;
+
+/// <summary>
+/// Represents a booking entity.
+/// </summary>
+public class Booking
+{
+    /// <summary>
+    /// Gets or sets the booking id.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the tenant name.
+    /// </summary>
+    public required string TenantName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the tenant passport number.
+    /// </summary>
+    public required string TenantPassportNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets the tenant phone number.
+    /// </summary>
+    public required string TenantPhoneNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets the booking start date.
+    /// </summary>
+    public DateTime Start { get; set; }
+
+    /// <summary>
+    /// Gets or sets the booking end date.
+    /// </summary>
+    public DateTime End { get; set; }
+
+    /// <summary>
+    /// Gets or sets the booking room id.
+    /// </summary>
+    public Guid RoomId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the room that is booked.
+    /// </summary>
+    public Room? Room { get; set; }
+}
