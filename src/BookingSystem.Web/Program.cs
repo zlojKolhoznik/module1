7dc782559b64ca21434fa23d189cb467af30d299
Roman Portianko <rportyanko10@gmail.com>
2025-03-14 11:37:59 +0200

    Added code linting and treating warnings as errors

--- src/BookingSystem.Web/Program.cs
+++ src/BookingSystem.Web/Program.cs
@@ -1,10 +1,14 @@
+// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c => 
+builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }));
 
 var app = builder.Build();
