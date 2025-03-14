42b0137753eec9de2d362c9d4d5c92424fc927b8
Roman Portianko <rportyanko10@gmail.com>
2025-03-14 10:38:11 +0200

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c => 
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
