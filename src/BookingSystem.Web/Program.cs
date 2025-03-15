d746da57e67aed68c39bf200d07da5c13b8f6e48
Roman Portianko <rportyanko10@gmail.com>
2025-03-15 17:03:05 +0200

    Refactor service registration and add extension methods for better organization

--- src/BookingSystem.Web/Program.cs
+++ src/BookingSystem.Web/Program.cs
@@ -2,36 +2,20 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-using BookingSystem.Data;
-using BookingSystem.Data.Abstractions;
-using BookingSystem.Data.Repositories;
-using BookingSystem.Data.Repositories.Abstractions;
-using BookingSystem.Data.UnitOfWork;
-using BookingSystem.Data.UnitOfWork.Abstractions;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
+using BookingSystem.Web.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }));
-builder.Services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IBookingRepository, BookingRepository>();
-builder.Services.AddScoped<IRoomsRepository, RoomsRepository>();
-builder.Services.AddScoped<IHotelsRepository, HotelsRepository>();
+builder.Services.AddDocumentation();
+builder.Services.AddDataAccess(builder.Configuration);
+builder.Services.AddDomainServices();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+    app.UseDocumentation();
 }
 
 app.UseHttpsRedirection();
